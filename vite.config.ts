598d5950299dc2696c4387d12ce060c742b0e2ec
Vlad Volkov <vlad.volkov@bndigital.co>
2023-02-07 19:05:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({command, mode, ssrBuild}) => {
+export default defineConfig(({ command }) => {
   return {
     base: process.env.APP_NAME ?
         `${process.env.APP_NAME}/` :
