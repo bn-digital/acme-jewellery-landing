497cc8e86af68859abb58bedcd255ee8fbc24545
Vlad Volkov <vlad.volkov@bndigital.co>
2023-02-07 14:52:08 +0200

    Added gh pages deployment
    Signed-off-by: Vlad Volkov <vlad.volkov@bndigital.co>

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,65 @@
-import './App.scss'
-import gsap from "gsap";
+import "./App.scss";
 
 // get other plugins:
-import ScrollTrigger from "gsap/ScrollTrigger";
 import {
-    addBasePlugins,
-    AssetManagerPlugin,
-    CameraViewPlugin, DiamondPlugin,
-    EasingFunctions,
-    GLTFAnimationPlugin, GroundPlugin, PickingPlugin,
-    timeout, TonemapPlugin,
-    ViewerApp
+  addBasePlugins,
+  AssetManagerPlugin,
+  CameraViewPlugin,
+  EasingFunctions,
+  timeout,
+  ViewerApp,
 } from "webgi";
-import * as THREE from 'three';
 import Header from "./components/header/Header";
 import Section01 from "./components/sections/Section01";
 import Section02 from "./components/sections/Section02";
 
-
 function App() {
-    async function setupViewer(){
+  async function setupViewer() {
+    // Initialize the viewer
+    const viewer = new ViewerApp({
+      canvas: document.getElementById("mcanvas") as HTMLCanvasElement,
+      useRgbm: true,
+    });
 
-        // Initialize the viewer
-        const viewer = new ViewerApp({
-            canvas: document.getElementById('mcanvas') as HTMLCanvasElement,
-            useRgbm: true,
-        })
+    // Add some plugins
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
 
-        // Add some plugins
-        const manager = await viewer.addPlugin(AssetManagerPlugin)
+    await addBasePlugins(viewer);
+    const camViews = viewer.getPlugin(CameraViewPlugin);
 
-        await addBasePlugins(viewer)
-        const camViews = viewer.getPlugin(CameraViewPlugin)
+    viewer.renderer.refreshPipeline();
 
-        viewer.renderer.refreshPipeline()
+    await manager.addFromPath("./assets/model/ring06.glb");
+    if (camViews) {
+      let view = camViews?.getCurrentCameraView(viewer.scene.activeCamera);
+      if (view) {
+        camViews?.camViews.push(view);
 
-        await manager.addFromPath("./assets/model/ring06.glb")
+        view = camViews?.getCurrentCameraView(viewer.scene.activeCamera);
+        view?.position.set(6, 4, 5);
+        camViews?.camViews.push(view);
 
-        let view = camViews.getCurrentCameraView(viewer.scene.activeCamera)
-        camViews.camViews.push(view)
+        view = camViews.getCurrentCameraView(viewer.scene.activeCamera);
+        view.position.set(-2, 4, 5);
+        view.target.set(0, 1, 0);
+        camViews.camViews.push(view);
+        camViews.animateOnScroll = true;
 
-        view = camViews.getCurrentCameraView(viewer.scene.activeCamera)
-        view.position.set(6,4,5)
-        camViews.camViews.push(view)
-
-        view = camViews.getCurrentCameraView(viewer.scene.activeCamera)
-        view.position.set(-2,4,5)
-        view.target.set(0,1,0)
-        camViews.camViews.push(view)
-        camViews.animateOnScroll = true
-
-
-        camViews.animDuration = 2000 // ms
+        camViews.animDuration = 2000; // ms
         // Allowed values: anticipate, backIn, backInOut, backOut, bounceIn, bounceInOut, bounceOut, circIn, circInOut, circOut, easeIn, easeInOut, easeOut, easeInOutSine
-        camViews.animEase = 'easeInOutSine' // default is easeInOutSine
+        camViews.animEase = "easeInOutSine"; // default is easeInOutSine
 
-        await camViews.animateToView(camViews.camViews[2])
-        await timeout(1500)
-        await camViews.animateToView(camViews.camViews[1], 2000, EasingFunctions.easeIn) // Override duration and easing
-        await timeout(1500)
-        await camViews.animateToView(camViews.camViews[0])
+        await camViews.animateToView(camViews.camViews[2]);
+        await timeout(1500);
+        await camViews.animateToView(
+          camViews.camViews[1],
+          2000,
+          EasingFunctions.easeIn
+        ); // Override duration and easing
+        await timeout(1500);
+        await camViews.animateToView(camViews.camViews[0]);
 
         // await camViews.animateAllViews() // Loop all views
-
 
         // Animate to a views without saving it
         // view = camViews.getCurrentCameraView(viewer.scene.activeCamera)
@@ -72,27 +69,27 @@ function App() {
         // view.position.set(2,4,5)
         // await camViews.animateToView(view, 3000, EasingFunctions.easeInOutSine)
         /// Chain further animations.
-
-
+      }
     }
-   setupViewer()
+  }
+
+  setupViewer();
   return (
     <div className="App">
-        <Header />
-        <Section01 />
-        <Section02 />
-        <div className="section">
-            <h1>{'Section 02'}</h1>
-        </div>
-        <div className="section">
-            <h1>{'Section 03'}</h1>
-        </div>
-        <div className="section">
-            <h1>{'Section 04'}</h1>
-        </div>
-
+      <Header />
+      <Section01 />
+      <Section02 />
+      <div className="section">
+        <h1>{"Section 02"}</h1>
+      </div>
+      <div className="section">
+        <h1>{"Section 03"}</h1>
+      </div>
+      <div className="section">
+        <h1>{"Section 04"}</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export {App}
